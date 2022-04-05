4d39695f434e7cd95f5d45c52771c53a14f99135
Rodrigo P <rportugal@cualit.com>
2022-04-04 22:39:16 -0300

    end first working version

--- app.js
+++ app.js
@@ -3,16 +3,50 @@ Vue.component('modal', {
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-          <h3>title</h3>
-          <div>body</div>
+          <header>
+            <p>{{ title }}</p>
+            <hr>
+          </header>
+          <section>
+            <i class="fa fa-5 fa-check green-circle white" aria-hidden="true"></i>
+          </section>
+          <section class="pt-0">
+            <p>Felicidades por completar</p>
+            <strong><slot></slot></strong>
+            <p>No olvides descargar tu certificado digital.  </p>
+          </section>
           <footer>
-            <button>Cerrar</button>
+            <button v-on:click="closeModal" class="btn-blue btn-100">Cerrar</button>
           </footer>
         </div>
       </div>
-    </div>`
+    </div>`,
+  props: ['title'],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    }
+  },
 })
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    cursos: [
+      'Curso de Desarrollo Web',
+      'Master en arquitectura de software',
+      'Git y Github for humans'
+    ],
+    showModal: false,
+  },
+  computed: {
+    randomCourse: function () {
+      return this.cursos[Math.floor(Math.random() * this.cursos.length)]
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
+  }
 })
